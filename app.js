79e848402c6b1ebe5dde4f71de4a9495ec5e7a9c
Kai <kai@kaikretschmann.de>
2016-08-21 11:20:48 +0200

    xmpp

--- app.js
+++ app.js
@@ -7,9 +7,109 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , Client = require('node-xmpp-client');
 
 var app = express();
+
+// XMPP stuff #1
+var client = new Client({
+//	  jid: 'tester@vocab.guru',
+//	  password: 'xxx',
+	  jid: 'admin@vocab.guru',
+	  password: 'g3h3im',
+	  reconnect: true
+	});
+
+var component = 'whatever';
+var x = 0;
+var old = x;
+var average = 0;
+setInterval(function () {
+  var n = x - old;
+  console.log(n, average);
+  average = (n + average) * 0.5;
+  old = x;
+}, 1e3);
+var interval;
+var firstMessage = true;
+var c = 0;
+
+client.on('stanza', function (stanza) {
+	  console.log('Received stanza: ', c++, stanza.toString());
+	  if (stanza.is('message') && stanza.attrs.type === 'chat' && !stanza.getChild('delay')) {
+	    clearInterval(interval);
+	    if (firstMessage) {
+	    	console.log('Someone started chatting …');
+	    }
+	    firstMessage = false;
+	    var i = parseInt(stanza.getChildText('body'), 10);
+	    x = i;
+	    var reply = new Client.Stanza('message', {
+	      to: stanza.attrs.from,
+	      from: stanza.attrs.to,
+	      type: 'chat'
+	    });
+	    reply.c('body').t(isNaN(i) ? 'i can count!' : ('' + (i + 1)));
+	    setTimeout(function () {
+	      client.send(reply);
+	    }, 321);
+	  }
+	});
+
+client.on('online', function () {
+  console.log('Client is online');
+  firstMessage = true;
+  // client.send('<presence/>');
+
+  var stanza = new Client.Stanza('iq', {type: 'set', id: 'reg1', to: 'vocab.guru'})
+  .c('query', {xmlns: 'jabber:iq:register'})
+  .c('username').t('user').up()
+  .c('password').t('1234');
+  client.send(stanza);
+
+/*
+  interval = setInterval(function () {
+    if (!firstMessage) {
+    	return;
+    }
+    //         firstMessage = false
+    console.log('Start chatting …');
+    var reply = new Client.Stanza('message', {
+      to: component,
+      type: 'chat'
+    });
+    reply.c('body').t('0');
+    client.send(reply);
+  }, 321);*/
+});
+
+client.on('offline', function () {
+  console.log('Client is offline');
+});
+
+client.on('connect', function () {
+  console.log('Client is connected');
+});
+
+client.on('reconnect', function () {
+  console.log('Client reconnects …');
+});
+
+client.on('disconnect', function (e) {
+  console.log('Client is disconnected', client.connection.reconnect, e);
+});
+
+client.on('error', function (e) {
+  console.error(e);
+  process.exit(1);
+});
+
+process.on('exit', function () {
+  client.end();
+});
+
+
 
 // all environments
 app.set('port', process.env.PORT || 3001);
