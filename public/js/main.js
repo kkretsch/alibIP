820f5c46846cf319682177358b651e180df3d096
Kai Kretschmann <kai@kaikretschmann.de>
2016-08-23 07:42:24 +0200

    bs form

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 
 	$('#name').on('keyup', function(e){
 //	if(e.keyCode === 13) {
-		var parameters = { search: $(this).val() };
+		var sTmp = $(this).val();
+		if(sTmp.length < 3) return;
+		var parameters = { search: sTmp };
 			$.get( '/u/unique', parameters, function(data) {
 				$('#nameStatus').html(data);
 			});
