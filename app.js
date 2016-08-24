2667fa83689da4d0936c3348182eda24480ffa37
Kai Kretschmann <kai@kaikretschmann.de>
2016-08-24 09:03:54 +0200

    First login and ping

--- app.js
+++ app.js
@@ -136,6 +136,12 @@ if ('development' === app.get('env')) {
 	next();
 });*/
 
+/*app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});*/
+
 app.get('/', function(req, res) {
 	res.redirect('http://blog.vocab.guru/');
 	res.end();
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,65 @@
+var cVocab = {
+	connection: null,
+	start_time: null,
+
+	log: function (msg) {
+        $('#log').append("<p>" + msg + "</p>");
+	},
+
+	send_ping: function (to) {
+        var ping = $iq({
+            to: to,
+            type: "get",
+            id: "ping1"}).c("ping", {xmlns: "urn:xmpp:ping"});
+
+        cVocab.log("Sending ping to " + to + ".");
+
+        cVocab.start_time = (new Date()).getTime();
+        cVocab.connection.send(ping);
+    },
+
+    handle_pong: function (iq) {
+        var elapsed = (new Date()).getTime() - cVocab.start_time;
+        cVocab.log("Received pong from server in " + elapsed + "ms.");
+
+        cVocab.connection.disconnect();
+        
+        return false;
+    }
+}; // class
+
+$(document).bind('connected', function () {
+	cVocab.log("Connection established.");
+    cVocab.connection.addHandler(cVocab.handle_pong, null, "iq", null, "ping1");
+    var domain = Strophe.getDomainFromJid(cVocab.connection.jid);
+    cVocab.send_ping(domain);
+});
+
+$(document).bind('disconnected', function () {
+	cVocab.log("Connection terminated.");
+    cVocab.connection = null;
+});
+
+
 $(document).ready(function() {
 
+	$('#loginform').submit(function(event) {
+		var sJid = $('#lname').val();
+		var sPwd = $('#pwd').val();
+
+		event.preventDefault();
+		var conn = new Strophe.Connection("http://vocab.guru:5280/http-bind");
+		conn.connect(sJid, sPwd, function (status) {
+			if (status === Strophe.Status.CONNECTED) {
+				$(document).trigger('connected');
+			} else if (status === Strophe.Status.DISCONNECTED) {
+				$(document).trigger('disconnected');
+			} // if
+		});
+
+	    cVocab.connection = conn;
+	});
+	
 	$('#name').on('keyup', function(e){
 		var sTmp = $(this).val();
 		if(sTmp.length < 3) {
