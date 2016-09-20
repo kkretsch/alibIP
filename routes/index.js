c9c0a4dbfa7af3a0667cab698f8f0bedfb1b674d
Kai <kai@kaikretschmann.de>
2016-09-20 17:10:32 +0200

    Home content for users

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@
 
 "use strict";
 
-exports.index = function(req, res){
-  res.render('pages/index', { title: 'Home' });
+exports.index = function(req, res) {
+	if(req.isAuthenticated()) {
+		res.render('pages/index_user', { title: 'Home user', user: req.user });
+	} else {
+		res.render('pages/index_anon', { title: 'Home guest' });
+	} // if
 };
