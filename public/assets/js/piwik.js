411e65c4402345fa66f91bd6338c2ea5513bee6a
Kai Kretschmann <kai@kaikretschmann.de>
2017-01-31 13:15:11 +0100

    stat

--- public/assets/js/piwik.js
+++ public/assets/js/piwik.js
@@ -1,11 +1,11 @@
-var idSite = 107;
+var idSite = 118;
 var piwikTrackingApiUrl = 'https://stat.myocastor.de/piwik.php';
 
 var _paq = _paq || [];
 _paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
 _paq.push(['setSiteId', idSite]);
 _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.vocab.guru"]);
-_paq.push(["setDomains", ["*.vocab.guru"]]);
+_paq.push(["setCookieDomain", "*.iplog.info"]);
+_paq.push(["setDomains", ["*.iplog.info"]]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
