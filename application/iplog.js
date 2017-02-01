542319f05952ec47c33248300449d0cfc546d2e4
Kai Kretschmann <kai@kaikretschmann.de>
2017-02-01 07:43:26 +0100

    return on error, ipv6 added

--- application/iplog.js
+++ application/iplog.js
@@ -27,22 +27,22 @@ module.exports = function(app, passport, myConnectionPool) {
 	// DynDNS API
 	app.get('/api/update/:username/:domain', function(req, res, next) {
 		var qPasswd = req.query.passwd;
-		var qIP = req.query.ip;
+		var qIPv4 = req.query.ip;
+		var qIPv6 = req.query.ipv6;
 
+		// Check for user, since we have no login session for api
 		myConnectionPool.query("SELECT * FROM users WHERE email=?", [req.username], function(err, rows) {
 			if(err) {
 				console.log("error getting user");
 				res.status(500);
 				res.send(RC_ERROR);
-				res.end();
-				next();
+				return res.end();
 			} // if backend error
 			if(0 === rows.length) {
 				console.log("no user found");
 				res.status(404);
 				res.send(RC_BADAUTH);
-				res.end();
-				next();
+				return res.end();
 			} // if user not found
 
 			var iduser = rows[0].id;
@@ -52,37 +52,44 @@ module.exports = function(app, passport, myConnectionPool) {
 				console.log("wrong password hash");
 				res.status(404);
 				res.send(RC_BADAUTH);
-				res.end();
-				next();
+				return res.end();
 			} // if pwd wrong
 
-			myConnectionPool.query("SELECT ipv4 FROM entries WHERE fkuser=? ORDER BY ts DESC LIMIT 1", [iduser], function(err, rows) {
+			// Get previous ipv6 and/or ipv6
+			myConnectionPool.query("SELECT ipv4,ipv6 FROM entries WHERE fkuser=? ORDER BY ts DESC LIMIT 1", [iduser], function(err, rows) {
 				if(err) {
 					console.log("error getting last ip");
 					res.status(500);
 					res.send(RC_ERROR);
-					res.end();
-					next();
+					return res.end();
 				} // if backend error
 
 				if(0 === rows.length) {
 					console.log("very first entry, OK");
 				} else {
-					var lastIP = rows[0].ipv4;
-					if(lastIP === qIP) {
-						res.send(RC_NOCHNG);
-						res.end();
-						next();
+					var lastIPv4 = rows[0].ipv4;
+					var lastIPv6 = rows[0].ipv6;
+					if(qIPv6) {
+						// Compare both IPs
+						if((lastIPv4 === qIPv4) && (lastIPv6 === qIPv6)) {
+							res.send(RC_NOCHNG);
+							return res.end();
+						}
+					} else {
+						// Compare only IPv4
+						if(lastIPv4 === qIPv4) {
+							res.send(RC_NOCHNG);
+							return res.end();
+						}
 					}
-				} // if multiple entries
+				}
 
-				myConnectionPool.query("INSERT INTO entries (fkuser,ipv4) VALUES(?,?)", [iduser,qIP], function(err, rows) {
+				myConnectionPool.query("INSERT INTO entries (fkuser,ipv4,ipv6) VALUES(?,?,?)", [iduser,qIPv4,qIPv6], function(err, rows) {
 					if(err) {
-						console.log("error inserting ip");
+						console.log("error inserting ip" + err);
 						res.status(500);
 						res.send(RC_ERROR);
-						res.end();
-						next();
+						return res.end();
 					} // if backend error
 
 					res.send(RC_OK);
